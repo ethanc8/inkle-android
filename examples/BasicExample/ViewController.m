fb37974b40a9a7272347e33b0778da70f785042e
ethanc8 <echaroenpitaks@gmail.com>
2024-08-13 17:09:47 -0500

    Add more stuff to the view controller

--- examples/BasicExample/ViewController.m
+++ examples/BasicExample/ViewController.m
@@ -8,15 +8,30 @@
 }
 
 - (void) viewDidLoad {
+    NSLog(@"-[ViewController viewDidLoad] called");
     [super viewDidLoad];
     self.view.backgroundColor = UIColor.whiteColor;
 
     // UIButtonTypeSystem isn't available
     loginButton = [UIButton buttonWithType: UIButtonTypeCustom];
     [loginButton setTitle: @"Login" forState: UIControlStateNormal];
+    loginButton.backgroundColor = UIColor.redColor;
     // loginButton.translatesAutoresizingMaskIntoConstraints = false
     [self.view addSubview: loginButton];
     [loginButton addTarget: self action: @selector(handleLoginTouchUpInside) forControlEvents: UIControlEventTouchUpInside];
+
+    nameTextField = [[UITextField alloc] initWithFrame:(CGRect){.origin = 0, .size = (NSSize){100, 100}}];
+    nameTextField.backgroundColor = UIColor.greenColor;
+    // nameTextField.placeholder = @"Login Name";
+    [self.view addSubview: nameTextField];
+
+    passwordTextField = [[UITextField alloc] initWithFrame:(CGRect){.origin = 0, .size = (NSSize){100, 100}}];
+    passwordTextField.backgroundColor = UIColor.blueColor;
+    [self.view addSubview: passwordTextField];
+
+    UIView* blackView = [[UITextField alloc] initWithFrame:(CGRect){.origin = 0, .size = (NSSize){100, 100}}];
+    blackView.backgroundColor = UIColor.blackColor;
+    [self.view addSubview: blackView];
 }
 
 - (void) handleLoginTouchUpInside {
